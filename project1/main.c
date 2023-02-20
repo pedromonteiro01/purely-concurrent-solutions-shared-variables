45bf1f47eca1d6b314c99ebf5a3591b4ac09f836
zepedrotrigo <josetrigo@ua.pt>
2023-02-20 02:58:31 +0000

    refactored code

--- project1/main.c
+++ project1/main.c
@@ -3,38 +3,56 @@
 
 #define NUM_LETTERS 26
 
-int main(int argc, char *argv[])
+long ParallelCharReader(char *text, int *letterFrequency)
 {
-    if (argc < 2) {
-        printf("%s <text> \n", argv[0]);
-        return 1; // error, no input text
-    }
-
-    int letter_count[NUM_LETTERS] = {0};
-    int bytes = 0;
-    char *text = argv[1];
+    long bytes = 0;
 
     // read input byte by byte  
     while (*text) {
-        bytes++;
-
         if (isalpha(*text)) { // checks whether a character is an alphabet
             int index = tolower(*text) - 'a';
-            letter_count[index]++;
+            letterFrequency[index]++;
         }
 
         text++;
+        bytes++;
     }
 
-    printf("Total bytes: %d\n", bytes);
-    printf("Letters:\n");
+    return bytes;
+}
+
+int main(int argc, char *argv[])
+{
+    // Validate input arguments
+    if (argc < 2) {
+        printf("%s <text> \n", argv[0]);
+        return 1;
+    }
+
+    /* -------------------- Declare and initialize variables -------------------- */
+    char *text = argv[1];
+    
+    //'{0}' initializes all the elements in the array to 0.
+    int letterFrequency[NUM_LETTERS] = {0};
+    /* -------------------------------------------------------------------------- */
+
+
+    /* -------------- Calls function to read chars from input text -------------- */
+    long bytes = ParallelCharReader(text, letterFrequency);
+
+
+    /* -------------------------------------------------------------------------- */
+    /*                               Prints results                               */
+    /* -------------------------------------------------------------------------- */
+    printf("Bytes read: %ld\n\nLetter Frequency:\n", bytes);
+
+    // Print letter frequency table
     for (int i = 0; i < NUM_LETTERS; i++) {
         char letter = 'a' + i;
-
-        // only prints letters that appears at least once
-        if (letter_count[i] != 0)
-            printf("%c: %d\n", letter, letter_count[i]);
+        printf("%c: %d, ", letter, letterFrequency[i]);
     }
+
+    printf("\n");
 
     return 0;
 }
