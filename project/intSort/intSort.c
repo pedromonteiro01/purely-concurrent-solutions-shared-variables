08c3a4b5d4adecd921688e111de368d3b1229a57
Pedro Monteiro <pmapm@ua.pt>
2023-03-11 16:42:42 +0000

    pass file through argv

--- project/intSort/intSort.c
+++ project/intSort/intSort.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 // Merges two sorted subarrays
-void merge(int* arr, int left, int middle, int right) {
+void merge(int *arr, int left, int middle, int right)
+{
     int i, j, k;
 
     // Find sizes of two subarrays
@@ -10,14 +11,16 @@ void merge(int* arr, int left, int middle, int right) {
     int right_size = right - middle;
 
     // Create temporary arrays for left and right subarrays
-    int* left_arr = malloc(left_size * sizeof(int));
-    int* right_arr = malloc(right_size * sizeof(int));
+    int *left_arr = malloc(left_size * sizeof(int));
+    int *right_arr = malloc(right_size * sizeof(int));
 
     // Copy data to temporary arrays
-    for (i = 0; i < left_size; i++) {
+    for (i = 0; i < left_size; i++)
+    {
         left_arr[i] = arr[left + i];
     }
-    for (j = 0; j < right_size; j++) {
+    for (j = 0; j < right_size; j++)
+    {
         right_arr[j] = arr[middle + 1 + j];
     }
 
@@ -25,11 +28,15 @@ void merge(int* arr, int left, int middle, int right) {
     i = 0;
     j = 0;
     k = left;
-    while (i < left_size && j < right_size) {
-        if (left_arr[i] <= right_arr[j]) {
+    while (i < left_size && j < right_size)
+    {
+        if (left_arr[i] <= right_arr[j])
+        {
             arr[k] = left_arr[i];
             i++;
-        } else {
+        }
+        else
+        {
             arr[k] = right_arr[j];
             j++;
         }
@@ -37,14 +44,16 @@ void merge(int* arr, int left, int middle, int right) {
     }
 
     // Copy remaining elements of left subarray, if any
-    while (i < left_size) {
+    while (i < left_size)
+    {
         arr[k] = left_arr[i];
         i++;
         k++;
     }
 
     // Copy remaining elements of right subarray, if any
-    while (j < right_size) {
+    while (j < right_size)
+    {
         arr[k] = right_arr[j];
         j++;
         k++;
@@ -56,8 +65,10 @@ void merge(int* arr, int left, int middle, int right) {
 }
 
 // Recursive function to sort array using merge sort
-void mergeSort(int* arr, int left, int right) {
-    if (left < right) {
+void mergeSort(int *arr, int left, int right)
+{
+    if (left < right)
+    {
         int middle = left + (right - left) / 2;
         mergeSort(arr, left, middle);
         mergeSort(arr, middle + 1, right);
@@ -66,46 +77,48 @@ void mergeSort(int* arr, int left, int right) {
 }
 
 // Function to print the sorted array
-void printArray(int* arr, int size) {
+void printArray(int *arr, int size)
+{
     int i;
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size; i++)
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-void validateSort(int* arr, int N) {
-    int i, N;
+void validateSort(int *arr, int N)
+{
+    int i;
 
-    for (i = 0; i < N - 1; i++) {
-        if (arr[i] > arr[i+1]) { 
-            printf ("Error in position %d between element %d and %d\n", i, arr[i], arr[i+1]);
-        break;
+    for (i = 0; i < N - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            printf("Error in position %d between element %d and %d\n", i, arr[i], arr[i + 1]);
+            break;
         }
         if (i == (N - 1))
-            printf ("Everything is OK!\n");
+            printf("Everything is OK!\n");
     }
 }
 
 // Main function to test merge sort algorithm
-int main() {
-    FILE *file;
-    char filename[50];
+int main(int argc, char *argv[])
+{
     int *arr, size, count;
 
-    printf("Enter the filename to open: ");
-    count = scanf("%49s", filename);
-
-    if (count != 1) {
-        printf("Error: invalid input\n");
-        exit(1);
+    if (argc != 2)
+    {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
     }
 
-    file = fopen(filename, "rb");
-
-    if (file == NULL) {
-        printf("Error: cannot open file\n");
-        exit(1);
+    FILE *file = fopen(argv[1], "rb");
+    if (file == NULL) // check if file is empty
+    {
+        printf("Error opening file\n");
+        return 1;
     }
 
     fseek(file, 0, SEEK_END);
@@ -114,28 +127,30 @@ int main() {
 
     arr = malloc(size * sizeof(int));
 
-    if (arr == NULL) {
+    if (arr == NULL)
+    {
         printf("Error: cannot allocate memory\n");
-        exit(1);
+        return 1;
     }
 
     count = fread(arr, sizeof(int), size, file);
 
-    if (count != size) {
+    if (count != size)
+    {
         printf("Error: could not read all integers from file\n");
-        exit(1);
+        return 1;
     }
 
     fclose(file);
 
-    //int arr_size = sizeof(arr) / sizeof(int);
+    // int arr_size = sizeof(arr) / sizeof(int);
 
     mergeSort(arr, 0, size - 1);
     validateSort(arr, size);
 
-    printf("\nSorted array is:\n");
+    printf("\nSorted array:\n");
     printArray(arr, size);
 
-        free(arr);
+    free(arr);
     return 0;
 }
